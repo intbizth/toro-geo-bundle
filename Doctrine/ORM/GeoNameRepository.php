03bd1fde15af2d400274512a3f3172576acbbc7e
liverbool <nukboon@gmail.com>
2017-03-25 12:04:36 +0700

    add geo findOr404

--- Doctrine/ORM/GeoNameRepository.php
+++ Doctrine/ORM/GeoNameRepository.php
@@ -3,6 +3,8 @@
 namespace Toro\Bundle\GeoBundle\Doctrine\ORM;
 
 use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Toro\Bundle\GeoBundle\Model\GeoNameInterface;
 use Toro\Bundle\GeoBundle\Repository\GeoNameRepositoryInterface;
 
 class GeoNameRepository extends EntityRepository implements GeoNameRepositoryInterface
@@ -115,5 +117,18 @@ class GeoNameRepository extends EntityRepository implements GeoNameRepositoryInt
             ->setParameter('locale', $locale)
             ->addOrderBy('o.left')
         ;
+    }
+
+    /**
+     * @param $id
+     * @return null|object|GeoNameInterface
+     */
+    public function findOr404($id)
+    {
+        if (!$geo = $this->find($id)) {
+            throw new NotFoundHttpException(sprintf('The "%s" has not been found', $id));
+        }
+
+        return $geo;
     }
 }
