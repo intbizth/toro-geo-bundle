711160c49bd6c88a781f5c6fbb37b60fa18b8340
liverbool <nukboon@gmail.com>
2017-04-10 08:07:11 +0700

    improve geo_tree filter

--- Grid/Filter/GeoTreeFilter.php
+++ Grid/Filter/GeoTreeFilter.php
@@ -4,6 +4,7 @@ namespace Toro\Bundle\GeoBundle\Grid\Filter;
 
 use Sylius\Component\Grid\Data\DataSourceInterface;
 use Sylius\Component\Grid\Filtering\FilterInterface;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
 use Toro\Bundle\GeoBundle\Model\GeoNameInterface;
 
 class GeoTreeFilter implements FilterInterface
@@ -11,12 +12,40 @@ class GeoTreeFilter implements FilterInterface
     const NAME = 'geo_tree';
 
     /**
+     * @var RepositoryInterface
+     */
+    private $repository;
+
+    /**
+     * @var FilterInterface
+     */
+    private $stringFilter;
+
+    public function __construct(RepositoryInterface $repository, FilterInterface $stringFilter)
+    {
+        $this->repository = $repository;
+        $this->stringFilter = $stringFilter;
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function apply(DataSourceInterface $dataSource, $name, $data, array $options)
     {
         if (!$data instanceof GeoNameInterface) {
-            throw new \InvalidArgumentException("Requred for type: " . GeoNameInterface::class);
+            if (is_numeric($data)) {
+                $data = $this->repository->find($data);
+            }
+
+            if (is_string($data)) {
+                return $this->stringFilter->apply($dataSource, $name, $data, array_replace_recursive([
+                    'fields' => [$options['trans']]
+                ], $options));
+            }
+
+            if (!$data instanceof GeoNameInterface) {
+                throw new \InvalidArgumentException("Requred for type: " . GeoNameInterface::class);
+            }
         }
 
         $expr = $dataSource->getExpressionBuilder();
